8c1b53ed20fb7be4d3c8900e592afe9863547827
Matheus Fernandes <matheussfernandesb@gmail.com>
2023-12-22 05:47:20 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import model.dao.DAOFactory;
 import model.dao.SellerDAO;
+import model.dao.impl.SellerDAOjdbc;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,13 +10,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
         SellerDAO SellerDAO = DAOFactory.createSellerDAO();
 
-        System.out.println(obj);
+        Seller seller = SellerDAO.findById(3);
 
         System.out.println(seller);
     }
--- src/model/dao/DAOFactory.java
+++ src/model/dao/DAOFactory.java
@@ -1,10 +1,10 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDAOjdbc;
 
 public class DAOFactory {
     public static SellerDAO createSellerDAO() {
-       return new SellerDAOjdbc();
-
+       return new SellerDAOjdbc(DB.getConnection());
     }
 }
