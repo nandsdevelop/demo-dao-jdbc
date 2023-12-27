77cca28a2e36b0259a8aaf3a9c1ecf5c2e4f2e44
Matheus Fernandes <matheussfernandesb@gmail.com>
2023-12-27 00:21:49 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,25 +11,45 @@ import java.util.Date;
 import java.util.List;
 
 
-
 public class Program {
     public static void main(String[] args) {
 
         SellerDAO SellerDAO = DAOFactory.SellerDAO();
 
         System.out.println("--- TEST 1: Seller findById ---");
+        System.out.println();
+
 
         Seller seller = SellerDAO.findById(3);
 
         System.out.println(seller);
 
+        System.out.println();
         System.out.println("--- SKIPPING 1 LINE... ---");
+        System.out.println();
+
 
         System.out.println("--- TEST 2: Seller findByDepartment ---");
+        System.out.println();
+
 
         Department department = new Department(2, null);
 
         List<Seller> list = SellerDAO.findByDepartment(department);
+
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println();
+        System.out.println("--- SKIPPING 1 LINE... ---");
+        System.out.println();
+
+        System.out.println("--- TEST 3: Seller findAll ---");
+        System.out.println();
+
+
+        list = SellerDAO.findAll();
 
         for (Seller obj : list) {
             System.out.println(obj);
