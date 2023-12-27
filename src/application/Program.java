ba245e3f43a91c2a177f722325f4f4eaea6a866e
Matheus Fernandes <matheussfernandesb@gmail.com>
2023-12-27 00:13:18 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,19 +6,33 @@ import model.dao.impl.SellerDAOjdbc;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.SQLOutput;
 import java.util.Date;
+import java.util.List;
 
-import static model.dao.DAOFactory.createSellerDAO;
+
 
 public class Program {
     public static void main(String[] args) {
 
-        SellerDAO SellerDAO = DAOFactory.createSellerDAO();
+        SellerDAO SellerDAO = DAOFactory.SellerDAO();
 
         System.out.println("--- TEST 1: Seller findById ---");
 
-        Seller seller = SellerDAO.findById(5);
+        Seller seller = SellerDAO.findById(3);
 
         System.out.println(seller);
+
+        System.out.println("--- SKIPPING 1 LINE... ---");
+
+        System.out.println("--- TEST 2: Seller findByDepartment ---");
+
+        Department department = new Department(2, null);
+
+        List<Seller> list = SellerDAO.findByDepartment(department);
+
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
--- src/model/dao/DAOFactory.java
+++ src/model/dao/DAOFactory.java
@@ -4,7 +4,7 @@ import db.DB;
 import model.dao.impl.SellerDAOjdbc;
 
 public class DAOFactory {
-    public static SellerDAO createSellerDAO() {
+    public static SellerDAO SellerDAO() {
        return new SellerDAOjdbc(DB.getConnection());
     }
 }
