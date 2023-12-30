94cc102664ca20a21cc49e30bee0013cd7a9221c
Matheus Fernandes <matheussfernandesb@gmail.com>
2023-12-30 01:10:36 -0300

    delete implementation

--- src/model/dao/impl/SellerDAOjdbc.java
+++ src/model/dao/impl/SellerDAOjdbc.java
@@ -76,11 +76,26 @@ public class SellerDAOjdbc implements SellerDAO {
 
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
         }
     }
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            st.setInt(1, id);
+            int rows = st.executeUpdate();
+            if (rows == 0) {
+                throw new SQLException("This id doesn't exist!");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
 
     }
 
